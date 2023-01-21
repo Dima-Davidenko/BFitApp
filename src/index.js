2645357d69d49c665bf12e6f7e078d4e0656d105
Dmytro Davidenko <dima080885@gmail.com>
2023-01-21 18:53:12 +0200

    routes bgfix

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ let theme = createTheme({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/BFitApp">
+      <BrowserRouter basename="BFitApp">
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
             <App />
